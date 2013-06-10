149b7c8f505e96c5a11001c0d2039eebb8916dd8
jemuillot <jemuillot@gmail.com>
2013-06-10 16:59:33 +0000

    Before Introducing new localized urls

--- src/jemuillot/pkg/Utilities/Andrutils.java
+++ src/jemuillot/pkg/Utilities/Andrutils.java
@@ -5,12 +5,28 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Locale;
 
 public class Andrutils {
-	
-	public static ArrayList<String> getHtmlLines(String urlString) {
+
+	public interface LocalizedStringChecker {
 		
+		String checkLocalizedString(Locale locale);
+
+		String checkLocalizedString(String string);
+	}
+
+	public interface StringChecker {
+		boolean check(final String string);
+	}
+
+	public LocalizedUrlHelper createLocalizedUrlHelper() {
+		return new LocalizedUrlHelper();
+	}
+
+	public static ArrayList<String> getHtmlLines(String urlString) {
+
 		ArrayList<String> ret = new ArrayList<String>();
 		try {
 			BufferedReader br = new BufferedReader(new InputStreamReader(
@@ -26,7 +42,7 @@ public class Andrutils {
 			return null;
 		}
 	}
-	
+
 	public static String getHtml(String urlString) {
 		try {
 			StringBuffer html = new StringBuffer();
@@ -44,11 +60,10 @@ public class Andrutils {
 		}
 	}
 
-
 	protected static boolean checkUrl(String urlString) {
 		try {
-	
-			 new BufferedReader(new InputStreamReader(
+
+			new BufferedReader(new InputStreamReader(
 					((HttpURLConnection) (new URL(urlString)).openConnection())
 							.getInputStream()));
 			return true;
@@ -56,15 +71,19 @@ public class Andrutils {
 			return false;
 		}
 	}
-	
 
-	public String getLocalizedString() {
+	public static String getLocalizedString(LocalizedStringChecker lsc) {
 
 		Locale lc = Locale.getDefault();
 
+		String ret = lsc.checkLocalizedString(lc);
+
+		if (ret != null)
+			return ret;
+
 		String fullRet = lc.toString().toLowerCase(Locale.US);
 
-		String ret = checkLocalizedString(fullRet);
+		ret = lsc.checkLocalizedString(fullRet);
 
 		if (ret != null)
 			return ret;
@@ -72,7 +91,7 @@ public class Andrutils {
 		ret = lc.getLanguage() + "_" + lc.getCountry().toLowerCase(Locale.US);
 
 		if (!ret.equals(fullRet)) {
-			ret = checkLocalizedString(ret);
+			ret = lsc.checkLocalizedString(ret);
 
 			if (ret != null)
 				return ret;
@@ -81,22 +100,80 @@ public class Andrutils {
 		ret = lc.getLanguage();
 
 		if (!ret.equals(fullRet)) {
-			ret = checkLocalizedString(ret);
+			ret = lsc.checkLocalizedString(ret);
 
 			if (ret != null)
 				return ret;
 		}
-
-		ret = checkLocalizedString("default");
+		
+		ret = lsc.checkLocalizedString("default");
 
 		if (ret != null)
 			return ret;
 
-		return checkLocalizedString("");
+		return lsc.checkLocalizedString("");
 	}
 
-	protected String checkLocalizedString(String string) {
-		return string;
+	public class LocalizedUrlHelper extends LocalizedStringHelper {
+
+		@Override
+		public boolean check(final String string) {
+			return Andrutils.checkUrl(string);
+		}
+
+		@Override
+		public String checkLocalizedString(Locale locale) {
+			return null;
+		}
+
+	}
+
+	public abstract class LocalizedStringHelper implements
+			LocalizedStringChecker, StringChecker {
+
+		protected String urlFormatted;
+		protected ArrayList<String> lanInfo;
+
+		public void setFormattedUrl(String url) {
+			urlFormatted = url;
+		}
+
+		public void setLanInfoFromUrl(String url) {
+			lanInfo = Andrutils.getHtmlLines(url);
+		}
+
+		public void setLanInfo(ArrayList<String> lanInfo) {
+			this.lanInfo = lanInfo;
+		}
+
+		public String getLocalizedString() {
+
+			return Andrutils.getLocalizedString(this);
+		}
+
+		public String checkLocalizedString(String string) {
+
+			if (lanInfo == null) {
+				string = String.format(urlFormatted, string);
+
+				if (check(string))
+					return string;
+			} else {
+
+				Iterator<String> it = lanInfo.iterator();
+
+				while (it.hasNext()) {
+					if (string.equals(it.next())) {
+						return String.format(urlFormatted, string);
+					}
+				}
+
+			}
+
+			return null;
+
+		}
+
 	}
 
 }
